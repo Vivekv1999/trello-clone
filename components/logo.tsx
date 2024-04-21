7e9f825e55e7a04712c709115ceee9d8e2d4be99
just-code001 <156091121+just-code001@users.noreply.github.com>
2024-04-21 08:54:40 +0530

    trello-clone initalization and add landing page

---
+++ components/logo.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p className="text-lg text-neutarl-700 pb-1">
+            Taskify 
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
---
+++ config/site.ts
@@ -0,0 +1,6 @@
+import { describe } from "node:test";
+
+export const siteConfig = {
+  name: "Taskify",
+  description: "COllaborate. mange projects, and reach new productivity peaks",
+};
